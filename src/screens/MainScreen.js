4100a51e3b6fb7bf5e3d61d0960fd979cc9381e2
Ersii <45428949+Ersii@users.noreply.github.com>
2020-10-26 11:38:28 +0100

    Update MainScreen.js

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -22,15 +22,50 @@ export default class MainScreen extends React.Component{
         this.props.navigation.navigate('Connect');
      }
 
-     render(){
+     /*render(){
         return <MainScreen
             sendDataInterval={1000}
             ip={this.props.navigation.state.params.ip}
-            {this.props}
-            {this.functions}
+            {...this.props }
+            {...this.functions }
         />
-     }
+     }*/
 
+     render(){
+         return(
+             <View style={{flex: 2}}>
+                 <View style={styles.topContainer}>
+                     <TouchableHighlight  >
+                         <Text> CAMERA </Text>
+                     </TouchableHighlight>
+                 </View>
+                 <View style={{flex: 1}}>
+                     <View style={styles.buttonRowContainer}>
+                         <TouchableHighlight style={styles.touchableContainer} activeOpacity={0.6} underlayColor="#DDDDDD" onPress={()=> console.log("lane assistant")}>
+                             <Text > Lane assistant </Text>
+                         </TouchableHighlight>
+                         <TouchableHighlight style={styles.touchableContainer} activeOpacity={0.6} underlayColor="#DDDDDD" onPress={()=> props.navigation.navigate('Connect')}>
+                             <Text>disconnect </Text>
+                         </TouchableHighlight>
+                         <TouchableHighlight style={styles.touchableContainer} activeOpacity={0.6} underlayColor="#DDDDDD" onPress={()=> props.navigation.navigate('Info')}>
+                             <Text  > driving info </Text>
+                         </TouchableHighlight>
+                     </View>
+                     <View style={styles.buttonRowContainer}>
+                         <TouchableHighlight  style={styles.minus}  activeOpacity={0.6} underlayColor="#DDDDDD" onPress={()=> console.log("power down")}>
+                             <Image source={require('../pictures/minus.png')} style={styles.image}/>
+                         </TouchableHighlight>
+                         <TouchableHighlight style={styles.touchableContainer} activeOpacity={0.6} underlayColor="#DDDDDD" onPress={()=> console.log("cruise control")}>
+                             <Text  > cruise control </Text>
+                         </TouchableHighlight>
+                         <TouchableHighlight  style={styles.plus} activeOpacity={0.6} underlayColor="#DDDDDD" onPress={()=> console.log("power up")}>
+                             <Image source={require('../pictures/plus.png')} style={styles.image}/>
+                         </TouchableHighlight>
+                     </View>
+                 </View>
+             </View>
+         )
+     }
 }
 
 //INTERFACE STRUCTURE
